c72ff50dda4692dde1cd777a49917e7177130663
Stephen S <pixonti3d@gmail.com>
2019-05-11 17:08:07 -0700

    Added documentation and license info
    Added documentation and license info.

--- main.php
+++ main.php
@@ -1,7 +1,26 @@
 <?PHP
+/*
+Portfolio Reviews
+https://github.com/pixonti
+ 
+Copyright (C) 2018 Stephen Studyvin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 include_once 'include/processes.php';
-//$Login_Process = new Login_Process;
-//$Login_Process->check_status($_SERVER['SCRIPT_NAME']);
+$Login_Process = new Login_Process;
+$Login_Process->check_status($_SERVER['SCRIPT_NAME']);
 
 
 if (isset($_POST['change_pass'])) {
