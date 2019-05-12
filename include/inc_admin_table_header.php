07cfdab05a568be4436124dd5fb15eff7f453bf4
Stephen S <pixonti3d@gmail.com>
2019-05-11 17:17:32 -0700

    Initial commit

---
+++ include/inc_admin_table_header.php
@@ -0,0 +1,13 @@
+	<table align="center" border="0" cellspacing="0" cellpadding="0" id="<?PHP echo $tableName ?>" class="ui striped seven column compact table admin_table" width="100%">
+    	<thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email Address</th>
+            <th scope="col">id number</th>
+            <th scope="col">Campus</th>
+            <th scope="col">Last Login</th>
+            <th scope="col">Level</th>
+            <th scope="col">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
